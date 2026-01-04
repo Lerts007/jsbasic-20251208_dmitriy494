051744dd0150d62e0aceb9a112d6aad2e5c3302a
Dmitry Strelnikov <oddff@mail.ru>
2026-01-04 16:17:42 +0300

    Учебный проект (пролог): Карусель

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,29 @@
 function initCarousel() {
-  // ваш код...
+  const container = document.querySelector(".carousel__inner");
+  const listSlides = document.querySelectorAll(".carousel__slide");
+  const btnNext = document.querySelector(".carousel__arrow_right");
+  const btnPrev = document.querySelector(".carousel__arrow_left");
+  const widthSlide = listSlides[0].offsetWidth;
+  let count = 0;
+  btnPrev.style.display = "none";
+
+  btnNext.addEventListener("click", slideOffsetNext);
+  btnPrev.addEventListener("click", slideOffsetPrev);
+
+  function slideOffsetNext() {
+    count++;
+    updateCarousel();
+  }
+
+  function slideOffsetPrev() {
+    count--;
+    updateCarousel();
+  }
+
+  function updateCarousel() {
+    btnNext.style.display = count == listSlides.length - 1 ? "none" : "flex";
+    btnPrev.style.display = count == 0 ? "none" : "flex";
+
+    container.style.transform = `translateX(-${count * widthSlide}px)`;
+  }
 }
